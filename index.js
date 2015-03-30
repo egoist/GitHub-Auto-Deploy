1cbcde6bd59f7f4e9fb64c9c09d5ee613a0bd00b
Kchan Zen <kchanzen@gmail.com>
2015-03-30 23:57:34 +0800

    update

--- index.js
+++ index.js
@@ -1,7 +1,25 @@
-var express = require('express');
-var app = express();
+var http = require('http')
+var createHandler = require('github-webhook-handler')
+var handler = createHandler({
+  path: '/snaply',
+  secret: process.env.HUB_SECRET
+})
 
-var snaply = function(req, res) {
+http.createServer(function(req, res) {
+  handler(req, res, function(err) {
+    res.statusCode = 404
+    res.end('no such location')
+  })
+}).listen(8282)
+
+handler.on('error', function(err) {
+  console.error('Error:', err.message)
+})
+
+handler.on('push', function(event) {
+  console.log('Received a push event for %s to %s',
+    event.payload.repository.name,
+    event.payload.ref)
   var exec = require('child_process').exec;
   exec('bash snaply.sh', function(error, stdout, stderr) {
     console.log('stdout: ', stdout);
@@ -11,10 +29,12 @@ var snaply = function(req, res) {
       console.log('exec error: ', error);
     }
   });
-}
+})
 
-app.get('/snaply', snaply);
-
-app.listen(8282, function() {
-  console.log('GitHub auto-deploy service is running...');
+handler.on('issues', function(event) {
+  console.log('Received an issue event for % action=%s: #%d %s',
+    event.payload.repository.name,
+    event.payload.action,
+    event.payload.issue.number,
+    event.payload.issue.title)
 })
